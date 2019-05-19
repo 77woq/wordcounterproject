58c8c3d67763d5fdd09ab330379eaaacaee8e20c
77woq <77woq@naver.com>
2019-05-19 15:47:25 +0900

    first commit

---
+++ views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+import operator
+
+# Create your views here.
+def home(request):
+    return render(request,'home.html')
+
+def about(request):
+    return render(request,'about.html')
+def count(request):
+
+    full_text=request.GET['fulltext']
+    word_list=full_text.split()
+    word_dictionary={}
+
+    for word in word_list:
+        if word in word_dictionary:
+            #include
+            word_dictionary[word]+=1
+        else:
+            #add to dictionary
+            word_dictionary[word]=1
+    sortedArr=sorted(word_dictionary.items(),key=operator.itemgetter(1),reverse=True)
+    
+    return render(request,'count.html',{'fulltext':full_text,'total':len(word_list),'dictionary':sortedArr,'first':sortedArr[0][1]})
